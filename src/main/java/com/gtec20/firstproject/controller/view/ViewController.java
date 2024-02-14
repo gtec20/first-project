e499b28bc07ea7e91745c6f435023c2f31417c77
ChoiseU <csw980928@gmail.com>
2024-02-14 13:47:12 +0900

    refactor : 매핑 변경

--- src/main/java/com/gtec20/firstproject/controller/view/ViewController.java
+++ src/main/java/com/gtec20/firstproject/controller/view/ViewController.java
@@ -1,14 +1,13 @@
 package com.gtec20.firstproject.controller.view;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
 
-    @GetMapping("")
-    public String login(Model model) {
+    @GetMapping("/login")
+    public String login() {
         return "page/login";
     }
 }
